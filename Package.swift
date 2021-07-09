ffc2d64c48d7c3a07a50b0380f7a699057f21a92
José Cuadros <jose.cuadros@data4life.care>
2021-07-09 12:41:25 +0200

    Set minimum deployment target to iOS 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AttributedText",
-    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)],
+    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)],
     products: [
         .library(
             name: "AttributedText",
